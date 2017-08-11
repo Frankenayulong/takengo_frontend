e8d308251f3df9630c1003de2046e2630dd22339
= <s3642807@student.rmit.edu.au>
2017-08-11 20:22:33 +1000

    limit error message in register form to 1

--- resources/views/components/modals/sign-up.blade.php
+++ resources/views/components/modals/sign-up.blade.php
@@ -19,21 +19,21 @@
                         <label for="signup-email" class="hide">Email</label>
                         <input ng-model="signup_information.email" type="email" class="form-control input-lg c-square" id="signup-email" placeholder="Email">
                         <span ng-if="signup_error.email" class="help-block">
-                            <strong ng-repeat="item in signup_error.message.email">@{{item}}</strong>
+                            <strong ng-repeat="item in signup_error.message.email | limitTo:1">@{{item}}</strong>
                         </span>
                     </div>
                     <div class="form-group" ng-class="signup_error.password ? 'has-error' : ''">
                         <label for="signup-password" class="hide">Password</label>
                         <input ng-model="signup_information.password" type="password" class="form-control input-lg c-square" id="signup-password" placeholder="Password">
                         <span ng-if="signup_error.password" class="help-block">
-                            <strong ng-repeat="item in signup_error.message.password">@{{item}}</strong>
+                            <strong ng-repeat="item in signup_error.message.password | limitTo:1">@{{item}}</strong>
                         </span>
                     </div>
                     <div class="form-group" ng-class="signup_error.password_confirmation ? 'has-error' : ''">
                         <label for="signup-password-confirmation" class="hide">Password Confirmation</label>
                         <input ng-model="signup_information.password_confirmation" type="password" class="form-control input-lg c-square" id="signup-password-confirmation" placeholder="Password Confirmation">
                         <span ng-if="signup_error.password_confirmation" class="help-block">
-                            <strong ng-repeat="item in signup_error.message.password_confirmation">@{{item}}</strong>
+                            <strong ng-repeat="item in signup_error.message.password_confirmation | limitTo:1">@{{item}}</strong>
                         </span>
                     </div>
                     <div class="form-group">
