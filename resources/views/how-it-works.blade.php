f90b1e2665c80fcd6ed781f971f43ea964801723
Nadya Safira <s3642868@student.rmit.edu.au>
2017-08-15 23:14:36 +1000

    add webpages

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -1,5 +1,70 @@
 @extends('layouts.master')
 
 @section('content')
+    <!-- BEGIN: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
+    <div class="c-layout-breadcrumbs-1 c-bgimage c-subtitle c-fonts-uppercase c-fonts-bold c-bg-img-center" style="background-image: url(../../assets/base/img/content/banner2.png); background-position:center 90%; background-size:cover;">
+        <div class="container">
+            <div class="c-page-title c-pull-left">
+                <h3 class="c-font-uppercase c-font-bold c-font-dark c-font-20 c-font-slim">FAQ</h3>
+                <h4 class="c-font-dark c-font-slim">
+                    Page Sub Title Goes Here			</h4>
+            </div>
+        </div>
+    </div><!-- END: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
+
+    <!-- BEGIN: CONTENT/STEPS/STEPS-1 -->
+    <div class="c-content-box c-size-md c-bg-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="c-content-title-1 c-margin-b-60">
+                        <h3 class="c-center c-font-uppercase c-font-bold">
+                            How We Optimize Your Business
+                        </h3>
+                        <div class="c-line-center"></div>
+                        <p class="c-center c-font-uppercase c-font-17">
+                            Lorem ipsum dolor sit amet consectetuer dolore
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 col-sm-6 wow animate fadeInLeft">
+                    <div class="c-content-step-1 c-opt-1">
+                        <div class="c-icon"><span class="c-hr c-hr-first"><span class="c-content-line-icon c-icon-14 c-theme"></span></span></div>
+                        <div class="c-title c-font-20 c-font-bold c-font-uppercase">1. First Important STEP</div>
+                        <div class="c-description c-font-17">
+                            Lorem ipsum dolor sit consectetuer
+                            adipiscing elit et diam nonummy.
+                        </div>
+                        <button class="btn c-btn-square c-theme-btn c-btn-border1-2x c-btn-uppercase c-btn-bold">EXPLORE</button>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 wow animate fadeInLeft" data-wow-delay="0.2s">
+                    <div class="c-content-step-1 c-opt-1">
+                        <div class="c-icon"><span class="c-hr"><span class="c-content-line-icon c-icon-21 c-theme"></span></span></div>
+                        <div class="c-title c-font-20 c-font-bold c-font-uppercase">2. Second Phase</div>
+                        <div class="c-description c-font-17">
+                            Lorem ipsum dolor sit consectetuer
+                            adipiscing elit et diam nonummy.
+                        </div>
+                        <button class="btn c-btn-square c-theme-btn c-btn-uppercase c-btn-bold">EXPLORE</button>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12 wow animate fadeInLeft" data-wow-delay="0.4s">
+                    <div class="c-content-step-1 c-opt-1">
+                        <div class="c-icon"><span class="c-hr c-hr-last"><span class="c-content-line-icon c-icon-32 c-theme"></span></span></div>
+                        <div class="c-title c-font-20 c-font-bold c-font-uppercase">3. Final Action</div>
+                        <div class="c-description c-font-17">
+                            Lorem ipsum dolor sit consectetuer
+                            adipiscing elit et diam nonummy.
+                        </div>
+                        <button class="btn c-btn-square c-theme-btn c-btn-uppercase c-btn-bold">EXPLORE</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- END: CONTENT/STEPS/STEPS-1 -->
 
 @endsection
--- resources/views/terms-and-conditions.blade.php
+++ resources/views/terms-and-conditions.blade.php
@@ -2,4 +2,141 @@
 
 @section('content')
 
+    <!-- BEGIN: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
+    <div class="c-layout-breadcrumbs-1 c-bgimage c-subtitle c-fonts-uppercase c-fonts-bold c-bg-img-center" style="background-image: url(../../assets/base/img/content/banner2.png); background-position:center 90%; background-size:cover;">
+        <div class="container">
+            <div class="c-page-title c-pull-left">
+                <h3 class="c-font-uppercase c-font-bold c-font-dark c-font-20 c-font-slim">FAQ</h3>
+                <h4 class="c-font-dark c-font-slim">
+                    Page Sub Title Goes Here			</h4>
+            </div>
+        </div><!-- END: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
+    </div>
+    <!-- BEGIN: PAGE CONTENT -->
+    <div class="c-content-box c-size-md ">
+        <div class="container">
+            <div class="cbp-panel">
+                <div class="c-content-title-1 c-margin-b-60">
+                    <h3 class="c-font-uppercase c-font-bold">Terms and Conditions ("Terms")</h3>
+                    <div class="c-line-left c-theme-bg"></div>
+                    <p class="c-font-uppercase c-font-12">Last updated: August 15, 2017</p>
+                    <p class="c-font-lowercase">Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before
+                        <br/> using the takengo.io website (the "Service") operated by Take N Go ("us", "we", or "our").
+                    </p>
+                    <p class="c-font-lowercase">
+                        Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms.
+                        <br/>These Terms apply to all visitors, users and others who access or use the Service.
+                    </p>
+                    <p class="c-font-lowercase">
+                        By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms
+                        <br/>then you may not access the Service. This Terms & Conditions agreement is licensed by TermsFeed to Take N Go.
+                    </p>
+
+                    <hr/>
+
+                    <h4 class="c-font-uppercase c-font-bold" style="font-size: 1.3em">Accounts</h4>
+                    <div class="c-line-left c-theme-bg"></div>
+                    <p class="c-font-lowercase">
+                        When you create an account with us, you must provide us information that is accurate, complete, and current at all times.
+                        <br/>Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.
+                    </p>
+                    <p class="c-font-lowercase">
+                        You are responsible for safeguarding the password that you use to access the Service and for any activities or actions
+                        <br/>under your password, whether your password is with our Service or a third-party service.
+                    </p>
+                    <p class="c-font-lowercase">
+                        You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of
+                        <br/>any breach of security or unauthorized use of your account.
+                    </p>
+
+                    <hr/>
+
+                    <h4 class="c-font-uppercase c-font-bold" style="font-size: 1.3em">Links To Other Web Sites</h4>
+                    <div class="c-line-left c-theme-bg"></div>
+                    <p class="c-font-lowercase">
+                        Our Service may contain links to third-party web sites or services that are not owned or controlled by Take N Go.
+                    </p>
+                    <p class="c-font-lowercase">
+                        Take N Go has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any
+                        <br/>third party web sites or services. You further acknowledge and agree that Take N Go shall not be responsible or liable,
+                        <br/>directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance
+                        <br/>on any such content, goods or services available on or through any such web sites or services.
+                    </p>
+                    <p class="c-font-lowercase">
+                        We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services
+                        <br/>that you visit.
+                    </p>
+
+                    <hr/>
+
+                    <h4 class="c-font-uppercase c-font-bold" style="font-size: 1.3em">Termination</h4>
+                    <div class="c-line-left c-theme-bg"></div>
+                    <p class="c-font-lowercase">
+                        We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason
+                        <br/>whatsoever, including without limitation if you breach the Terms.
+                    </p>
+                    <p class="c-font-lowercase">
+                        All provisions of the Terms which by their nature should survive termination shall survive termination, including,
+                        <br/>without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
+                    </p>
+                    <p class="c-font-lowercase">
+                        We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever,
+                        <br/>including without limitation if you breach the Terms.
+                    </p>
+                    <p class="c-font-lowercase">
+                        Upon termination, your right to use the Service will immediately cease. If you wish to terminate your account, you may
+                        <br/>simply discontinue using the Service.
+                    </p>
+                    <p class="c-font-lowercase">
+                        All provisions of the Terms which by their nature should survive termination shall survive termination, including,
+                        <br/>without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
+                    </p>
+
+                    <hr/>
+
+                    <h4 class="c-font-uppercase c-font-bold" style="font-size: 1.3em">Governing Law</h4>
+                    <div class="c-line-left c-theme-bg"></div>
+                    <p class="c-font-lowercase">
+                        These Terms shall be governed and construed in accordance with the laws of Victoria, Australia, without regard to its
+                        <br/>conflict of law provisions.
+                    </p>
+                    <p class="c-font-lowercase">
+                        Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any
+                        <br/>provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will
+                        <br/>remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and
+                        <br/>replace any prior agreements we might have between us regarding the Service.
+                    </p>
+
+                    <hr/>
+
+                    <h4 class="c-font-uppercase c-font-bold" style="font-size: 1.3em">Changes</h4>
+                    <div class="c-line-left c-theme-bg"></div>
+                    <p class="c-font-lowercase">
+                        We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we
+                        <br/>will try to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will
+                        <br/>be determined at our sole discretion.
+                    </p>
+                    <p class="c-font-lowercase">
+                        By continuing to access or use our Service after those revisions become effective, you agree to be bound by the
+                        <br/>revised terms. If you do not agree to the new terms, please stop using the Service.
+                    </p>
+
+                    <hr/>
+
+                    <h4 class="c-font-uppercase c-font-bold" style="font-size: 1.3em">Changes</h4>
+                    <div class="c-line-left c-theme-bg"></div>
+                    <p class="c-font-lowercase">
+                        If you have any questions about these Terms, please contact us.
+                    </p>
+
+                    <hr/>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END: PAGE CONTENT -->
+
 @endsection
