606461a277667c6ce06ef3ee092fe1fbd3dd3821
kendrick <s3642868@student.rmit.edu.au>
2017-08-15 17:09:13 +1000

    add faq.js in gulp

---
+++ public/js/faq-min.js
@@ -0,0 +1,1 @@
+!function(e,i,t,n){"use strict";jQuery("#grid-container").cubeportfolio({filters:"#filters-container",defaultFilter:"*",animationType:"sequentially",gridAdjustment:"responsive",displayType:"default",caption:"expand",mediaQueries:[{width:1,cols:1}],gapHorizontal:0,gapVertical:0})}(0,window,document);
