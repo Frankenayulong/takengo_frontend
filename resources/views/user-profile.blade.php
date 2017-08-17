73375cf8f29c6e44c8f7766a796f87a12d12bddb
Nadya Safira <s3642868@student.rmit.edu.au>
2017-08-17 13:05:17 +1000

    edit user profile page

--- resources/views/user-profile.blade.php
+++ resources/views/user-profile.blade.php
@@ -2,19 +2,20 @@
 
 @section('content')
 
+    <!-- START BREADCRUMBS -->
+    <div class="c-layout-breadcrumbs-1 c-bgimage-full  c-centered  c-fonts-uppercase c-fonts-bold   c-bg-img-center" style="background-image: url({{asset('assets/base/img/content/backgrounds/bg-47.jpg')}})">
+        <div class="container">
+            <div class="c-page-title c-pull-left">
+                <h2 class="c-font-uppercase c-font-bold c-font-white c-font-35 c-font-slim c-opacity-09">My Dashboard</h2>
+            </div>
+            <ul class="c-page-breadcrumbs c-theme-nav c-pull-right c-fonts-regular">
+                <li><a href="{{url('/')}}" class="c-font-white">Home</a></li>
+                <li class="c-font-white">/</li>
+                <li class="c-state_active c-font-white">My Dashboard</li>
+            </ul>
+        </div>
+    </div>
+    <!-- END BREADCRUMBS -->
+
 @endsection
 
-<!-- START BREADCRUMBS -->
-<div class="c-layout-breadcrumbs-1 c-bgimage-full  c-centered  c-fonts-uppercase c-fonts-bold   c-bg-img-center" style="background-image: url({{asset('assets/base/img/content/backgrounds/bg-47.jpg')}})">
-    <div class="container">
-        <div class="c-page-title c-pull-left">
-            <h2 class="c-font-uppercase c-font-bold c-font-white c-font-35 c-font-slim c-opacity-09">My Dashboard</h2>
-        </div>
-        <ul class="c-page-breadcrumbs c-theme-nav c-pull-right c-fonts-regular">
-            <li><a href="{{url('/')}}" class="c-font-white">Home</a></li>
-            <li class="c-font-white">/</li>
-            <li class="c-state_active c-font-white">My Dashboard</li>
-        </ul>
-    </div>
-</div>
-<!-- END BREADCRUMBS -->
